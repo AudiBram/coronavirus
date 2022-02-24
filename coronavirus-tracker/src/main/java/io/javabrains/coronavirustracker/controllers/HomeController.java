3e2326b5de036f5219c5116e440edefd5c148e30
Audi Bramastya Oroh <audibram14@gmail.com>
2022-02-24 12:50:25 +0700

    add projects

---
+++ coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package io.javabrains.coronavirustracker.controllers;
+
+import io.javabrains.coronavirustracker.models.LocationStats;
+import io.javabrains.coronavirustracker.services.DataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+
+@Controller("/")
+public class HomeController {
+
+    @Autowired
+    DataService dataService;
+
+    @GetMapping("home")
+    public String home(Model model) {
+        List<LocationStats> allStats = dataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(LocationStats::getLatestTotalCases).sum();
+        int totalNewCases = allStats.stream().mapToInt(LocationStats::getDiffFromPrevDay).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home-page";
+    }
+}
